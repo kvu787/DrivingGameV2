ee82d72bd870f34f5f8bbd6b2d9811b9b257b763
Kevin Vu <kevin.m.vu@gmail.com>
2025-08-01 23:10:36 -0700

    temp wip

--- Unity/Assets/Scripts/UI.cs
+++ Unity/Assets/Scripts/UI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,19 +8,30 @@ namespace DrivingGameV2 {
         public static string LapTimesFilePath;
 
         void Awake() {
+            //Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
             string prefix = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss-fff");
             LapTimesFilePath = $"{Application.persistentDataPath}/{prefix} LapTimes.txt".Replace("/", "\\");
         }
 
-        void OnEnable() {
-            string debugLogFilePath = $"{Application.persistentDataPath}/Player.log".Replace("/", "\\");
-            string text =
-                $"DEBUG LOG:\n" +
-                $"{debugLogFilePath}\n" +
-                $"\n" +
-                $"LAP TIMES:\n" +
-                $"{LapTimesFilePath}";
-            this.GetComponent<UIDocument>().rootVisualElement.Q<Label>("topLeftText").text = text;
+        //void OnEnable() {
+        //    string debugLogFilePath = $"{Application.persistentDataPath}/Player.log".Replace("/", "\\");
+        //    string text =
+        //        $"DEBUG LOG:\n" +
+        //        $"{debugLogFilePath}\n" +
+        //        $"\n" +
+        //        $"LAP TIMES:\n" +
+        //        $"{LapTimesFilePath}";
+        //    this.GetComponent<UIDocument>().rootVisualElement.Q<Label>("topLeftText").text = text;
+        //}
+
+        void Update() {
+            float fps = 1f / Time.deltaTime;
+            List<string> lines = new() {
+                $"FPS: {fps}",
+                $"Screen.fullScreen: {Screen.fullScreen}",
+                $"Screen.fullScreenMode: {Screen.fullScreenMode}",
+            };
+            this.GetComponent<UIDocument>().rootVisualElement.Q<Label>("topLeftText").text = string.Join("\n", lines);
         }
     }
 }
